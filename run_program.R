5a5a005bc724fc2f671d97d6561f098c675a6ebf
mingkaijiang <shanyujordan512@gmail.com>
2018-05-07 11:10:03 +1000

    ran some preliminary stuff

--- run_program.R
+++ run_program.R
@@ -21,3 +21,84 @@ colnames(myDF) <- c("Ref", "Variable", "Unit", "aCO2", "eCO2", "aP", "eP", "P_un
                     "Species", "Soil_weight", "Pot_volume", "Experiment_duration",
                     "eC_by_aC", "eP_by_aP", "aCeP_by_aCaP", "eCaP_by_aCaP", "eCeP_by_aCaP",
                     "Additive_interaction", "Multiplicative_interaction")
+
+### change class of numeric variables
+myDF$aCO2 <- as.numeric(myDF$aCO2)
+myDF$eCO2 <- as.numeric(myDF$eCO2)
+myDF$aP <- as.numeric(myDF$aP)
+myDF$eP <- as.numeric(myDF$aP)
+myDF$Sample_size <- as.numeric(myDF$Sample_size)
+myDF$aCaP_mean <- as.numeric(myDF$aCaP_mean)
+myDF$aCeP_mean <- as.numeric(myDF$aCeP_mean)
+myDF$eCaP_mean <- as.numeric(myDF$eCaP_mean)
+myDF$eCeP_mean <- as.numeric(myDF$eCeP_mean)
+
+myDF$aCaP_plus <- as.numeric(myDF$aCaP_plus)
+myDF$aCeP_plus <- as.numeric(myDF$aCeP_plus)
+myDF$eCaP_plus <- as.numeric(myDF$eCaP_plus)
+myDF$eCeP_plus <- as.numeric(myDF$eCeP_plus)
+
+myDF$aCaP_minus <- as.numeric(myDF$aCaP_minus)
+myDF$aCeP_minus <- as.numeric(myDF$aCeP_minus)
+myDF$eCaP_minus <- as.numeric(myDF$eCaP_minus)
+myDF$eCeP_minus <- as.numeric(myDF$eCeP_minus)
+
+myDF$aCaP_variance <- as.numeric(myDF$aCaP_variance)
+myDF$aCeP_variance <- as.numeric(myDF$aCeP_variance)
+myDF$eCaP_variance <- as.numeric(myDF$eCaP_variance)
+myDF$eCeP_variance <- as.numeric(myDF$eCeP_variance)
+
+myDF$leafP_conc_aCaP <- as.numeric(myDF$leafP_conc_aCaP)
+myDF$leafP_conc_eCaP <- as.numeric(myDF$leafP_conc_eCaP)
+myDF$leafP_conc_aCeP <- as.numeric(myDF$leafP_conc_aCeP)
+myDF$leafP_conc_eCeP <- as.numeric(myDF$leafP_conc_eCeP)
+
+myDF$Experiment_duration <- as.numeric(myDF$Experiment_duration)
+
+
+### Get variable names
+var.names <- unique(myDF$Variable)
+
+### Prepare df to store variance information
+stDF <- data.frame(var.names, NA, NA, NA, NA, NA)
+colnames(stDF) <- c("Variable","aCaP", "aCeP", "eCaP", "eCeP", "Sample_size")
+
+### assign variance and sample size
+for (i in 1:length(var.names)) {
+    subDF <- subset(myDF, Variable==var.names[i])
+    
+    ### calculate median for each sd to fill the NAs
+    stDF[stDF$Variable == var.names[i], "aCaP"] <- median(subDF$aCaP_variance, na.rm=T)
+    stDF[stDF$Variable == var.names[i], "aCeP"] <- median(subDF$aCeP_variance, na.rm=T)
+    stDF[stDF$Variable == var.names[i], "eCaP"] <- median(subDF$eCaP_variance, na.rm=T)
+    stDF[stDF$Variable == var.names[i], "eCeP"] <- median(subDF$eCeP_variance, na.rm=T)
+    stDF[stDF$Variable == var.names[i], "Sample_size"] <- median(subDF$Sample_size, na.rm=T)
+    
+}
+
+for (i in 1:length(var.names)) {
+    myDF[myDF$Variable==var.names[i], "aCaP_variance"][is.na(myDF[myDF$Variable==var.names[i],"aCaP_variance"])] <- stDF[stDF$Variable == var.names[i], "aCaP"]
+    myDF[myDF$Variable==var.names[i], "aCeP_variance"][is.na(myDF[myDF$Variable==var.names[i],"aCeP_variance"])] <- stDF[stDF$Variable == var.names[i], "aCeP"]
+    myDF[myDF$Variable==var.names[i], "eCaP_variance"][is.na(myDF[myDF$Variable==var.names[i],"eCaP_variance"])] <- stDF[stDF$Variable == var.names[i], "eCaP"]
+    myDF[myDF$Variable==var.names[i], "eCeP_variance"][is.na(myDF[myDF$Variable==var.names[i],"eCeP_variance"])] <- stDF[stDF$Variable == var.names[i], "eCeP"]
+    
+    myDF[myDF$Variable==var.names[i], "Sample_size"][is.na(myDF[myDF$Variable==var.names[i],"Sample_size"])] <- stDF[stDF$Variable == var.names[i], "Sample_size"]
+    
+}
+
+### Recalculate +sd and -sd
+myDF$aCaP_plus <- myDF$aCaP_mean + myDF$aCaP_mean * myDF$aCaP_variance
+myDF$aCaP_minus <- myDF$aCaP_mean - myDF$aCaP_mean * myDF$aCaP_variance
+
+myDF$aCeP_plus <- myDF$aCeP_mean + myDF$aCeP_mean * myDF$aCeP_variance
+myDF$aCeP_minus <- myDF$aCeP_mean - myDF$aCeP_mean * myDF$aCeP_variance
+
+myDF$eCaP_plus <- myDF$eCaP_mean + myDF$eCaP_mean * myDF$eCaP_variance
+myDF$eCaP_minus <- myDF$eCaP_mean - myDF$eCaP_mean * myDF$eCaP_variance
+
+myDF$eCeP_plus <- myDF$eCeP_mean + myDF$eCeP_mean * myDF$eCeP_variance
+myDF$eCeP_minus <- myDF$eCeP_mean - myDF$eCeP_mean * myDF$eCeP_variance
+
+
+### Plotting
+with(myDF[myDF$Variable == "Total biomass",], plot(eCaP_by_aCaP~eC_by_aC, col=eP_by_aP))
