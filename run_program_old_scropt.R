609eff18541a9e0f162d56f6d84cc38a4da56880
mingkaijiang <shanyujordan512@gmail.com>
2018-08-27 16:30:17 +1000

    updating with new scripts and data

--- run_program_old_scropt.R
+++ run_program_old_scropt.R
@@ -2,30 +2,135 @@
 ##### Author: Mingkai Jiang
 
 ##### Master script
+
+
 #### library
-require(ggplot2)
+require(readxl)
+
 
 #### check input files
-myDF <- read.csv("data/P_by_CO2_data_cleaned_no_eq_V1.csv")
+myDF <- read_excel("data/PxCO2_literature_raw.xlsx", sheet = "biomass",
+                   skip=2, col_names=F)
+myDF <- as.data.frame(myDF)
+colnames(myDF) <- c("Ref", "Variable", "Unit", "aCO2", "eCO2", "aP", "eP", "P_unit", "P_add_freq","Sample_size", 
+                    "aCaP_mean", "aCaP_plus", "aCaP_minus","aCaP_variance",
+                    "aCeP_mean", "aCeP_plus", "aCeP_minus","aCeP_variance",
+                    "eCaP_mean", "eCaP_plus", "eCaP_minus","eCaP_variance",
+                    "eCeP_mean", "eCeP_plus", "eCeP_minus","eCeP_variance",
+                    "leafP_conc_aCaP", "leafP_conc_aCeP", "leafP_conc_eCaP", "leafP_conc_eCeP",
+                    "Species", "Soil_weight", "Pot_volume", "Experiment_duration",
+                    "eC_by_aC", "eP_by_aP", "aCeP_by_aCaP", "eCaP_by_aCaP", "eCeP_by_aCaP",
+                    "Additive_interaction", "Multiplicative_interaction")
 
-myDF$Interaction_additive <- as.numeric(as.character(myDF$Interaction_additive))
-myDF$Interaction_multiplicative <- as.numeric(as.character(myDF$Interaction_multiplicative))
+### change class of numeric variables
+myDF$aCO2 <- as.numeric(myDF$aCO2)
+myDF$eCO2 <- as.numeric(myDF$eCO2)
+myDF$aP <- as.numeric(myDF$aP)
+myDF$eP <- as.numeric(myDF$eP)
+myDF$Sample_size <- as.numeric(myDF$Sample_size)
+myDF$aCaP_mean <- as.numeric(myDF$aCaP_mean)
+myDF$aCeP_mean <- as.numeric(myDF$aCeP_mean)
+myDF$eCaP_mean <- as.numeric(myDF$eCaP_mean)
+myDF$eCeP_mean <- as.numeric(myDF$eCeP_mean)
 
-### Calculate how much CO2 rise there is
-myDF$Trt_eC_by_aC <- myDF$Trt_eCO2/myDF$Trt_aCO2
+myDF$aCaP_plus <- as.numeric(myDF$aCaP_plus)
+myDF$aCeP_plus <- as.numeric(myDF$aCeP_plus)
+myDF$eCaP_plus <- as.numeric(myDF$eCaP_plus)
+myDF$eCeP_plus <- as.numeric(myDF$eCeP_plus)
 
-### Calculate how much P reduction there is
-myDF$Trt_P_reduction <- (myDF$Trt_eP - myDF$Trt_aP) / myDF$Trt_eP
+myDF$aCaP_minus <- as.numeric(myDF$aCaP_minus)
+myDF$aCeP_minus <- as.numeric(myDF$aCeP_minus)
+myDF$eCaP_minus <- as.numeric(myDF$eCaP_minus)
+myDF$eCeP_minus <- as.numeric(myDF$eCeP_minus)
+
+myDF$aCaP_variance <- as.numeric(myDF$aCaP_variance)
+myDF$aCeP_variance <- as.numeric(myDF$aCeP_variance)
+myDF$eCaP_variance <- as.numeric(myDF$eCaP_variance)
+myDF$eCeP_variance <- as.numeric(myDF$eCeP_variance)
+
+myDF$leafP_conc_aCaP <- as.numeric(myDF$leafP_conc_aCaP)
+myDF$leafP_conc_eCaP <- as.numeric(myDF$leafP_conc_eCaP)
+myDF$leafP_conc_aCeP <- as.numeric(myDF$leafP_conc_aCeP)
+myDF$leafP_conc_eCeP <- as.numeric(myDF$leafP_conc_eCeP)
+
+myDF$Experiment_duration <- as.numeric(myDF$Experiment_duration)
+
+### Redo calculations
+myDF$eC_by_aC <- myDF$eCO2/myDF$aCO2
+myDF$eP_by_aP <- myDF$eP/myDF$aP
+
+myDF$aCeP_by_aCaP <- myDF$aCeP_mean/myDF$aCaP_mean
+myDF$eCaP_by_aCaP <- myDF$eCaP_mean/myDF$aCaP_mean
+myDF$eCeP_by_aCaP <- myDF$eCeP_mean/myDF$aCaP_mean
+
+myDF$Multiplicative_interaction <- (myDF$eCeP_mean/myDF$eCaP_mean)/(myDF$aCeP_mean/myDF$aCaP_mean) - 1
+myDF$Additive_interaction <- (eCeP_by_aCaP-1) - (eCaP_by_aCaP-1) - (aCeP_by_aCaP - 1)
+
+
+### Get variable names
+var.names <- unique(myDF$Variable)
+
+### Prepare df to store variance information
+stDF <- data.frame(var.names, NA, NA, NA, NA, NA)
+colnames(stDF) <- c("Variable","aCaP", "aCeP", "eCaP", "eCeP", "Sample_size")
+
+### assign variance and sample size
+for (i in 1:length(var.names)) {
+    subDF <- subset(myDF, Variable==var.names[i])
+    
+    ### calculate median for each sd to fill the NAs
+    stDF[stDF$Variable == var.names[i], "aCaP"] <- median(subDF$aCaP_variance, na.rm=T)
+    stDF[stDF$Variable == var.names[i], "aCeP"] <- median(subDF$aCeP_variance, na.rm=T)
+    stDF[stDF$Variable == var.names[i], "eCaP"] <- median(subDF$eCaP_variance, na.rm=T)
+    stDF[stDF$Variable == var.names[i], "eCeP"] <- median(subDF$eCeP_variance, na.rm=T)
+    stDF[stDF$Variable == var.names[i], "Sample_size"] <- median(subDF$Sample_size, na.rm=T)
+    
+}
+
+for (i in 1:length(var.names)) {
+    myDF[myDF$Variable==var.names[i], "aCaP_variance"][is.na(myDF[myDF$Variable==var.names[i],"aCaP_variance"])] <- stDF[stDF$Variable == var.names[i], "aCaP"]
+    myDF[myDF$Variable==var.names[i], "aCeP_variance"][is.na(myDF[myDF$Variable==var.names[i],"aCeP_variance"])] <- stDF[stDF$Variable == var.names[i], "aCeP"]
+    myDF[myDF$Variable==var.names[i], "eCaP_variance"][is.na(myDF[myDF$Variable==var.names[i],"eCaP_variance"])] <- stDF[stDF$Variable == var.names[i], "eCaP"]
+    myDF[myDF$Variable==var.names[i], "eCeP_variance"][is.na(myDF[myDF$Variable==var.names[i],"eCeP_variance"])] <- stDF[stDF$Variable == var.names[i], "eCeP"]
+    
+    myDF[myDF$Variable==var.names[i], "Sample_size"][is.na(myDF[myDF$Variable==var.names[i],"Sample_size"])] <- stDF[stDF$Variable == var.names[i], "Sample_size"]
+    
+}
+
+### Recalculate +sd and -sd
+myDF$aCaP_plus <- myDF$aCaP_mean + myDF$aCaP_mean * myDF$aCaP_variance
+myDF$aCaP_minus <- myDF$aCaP_mean - myDF$aCaP_mean * myDF$aCaP_variance
+
+myDF$aCeP_plus <- myDF$aCeP_mean + myDF$aCeP_mean * myDF$aCeP_variance
+myDF$aCeP_minus <- myDF$aCeP_mean - myDF$aCeP_mean * myDF$aCeP_variance
+
+myDF$eCaP_plus <- myDF$eCaP_mean + myDF$eCaP_mean * myDF$eCaP_variance
+myDF$eCaP_minus <- myDF$eCaP_mean - myDF$eCaP_mean * myDF$eCaP_variance
+
+myDF$eCeP_plus <- myDF$eCeP_mean + myDF$eCeP_mean * myDF$eCeP_variance
+myDF$eCeP_minus <- myDF$eCeP_mean - myDF$eCeP_mean * myDF$eCeP_variance
+
+myDF$aCeP_by_aCaP <- as.numeric(myDF$aCeP_by_aCaP)
+myDF$eCaP_by_aCaP <- as.numeric(myDF$eCaP_by_aCaP)
+myDF$eCeP_by_aCaP <- as.numeric(myDF$eCeP_by_aCaP)
+
+write.csv(myDF, "data/test_biomass.csv")
 
 ### Plotting
+require(ggplot2)
 
-### summary histgram of treatments
-hist(myDF$Trt_aCO2, xlab = "aCO2 (ppm)", main=NA)
-hist(myDF$Trt_eCO2, xlab = "eCO2 (ppm)", main=NA)
-hist(myDF$Trt_eC_by_aC, xlab = "eCO2/aCO2", main=NA)
-hist(myDF$Trt_P_reduction, xlab = "eP/aP", main=NA)
+#### Total biomass
+pdf("output/summary_total_biomass.pdf")
+### plot some basic information
+par(mfrow=c(2,2))
+hist(myDF$aCO2, xlab = "aCO2 (ppm)", main=NA)
+hist(myDF$eCO2, xlab = "eCO2 (ppm)", main=NA)
+hist(myDF$eC_by_aC, xlab = "eCO2/aCO2", main=NA)
+hist(myDF$eP_by_aP, xlab = "eP/aP", main=NA)
+legend("topright", paste0("n=", nrow(myDF)))
 
 ### Plot treatment ratio
+par(mfrow=c(1,2))
 test <- subset(myDF, eP_by_aP <= 100 & Variable == "Total biomass")
 
 hist(test$eP_by_aP, xlab = "eP/aP", main=NA)
