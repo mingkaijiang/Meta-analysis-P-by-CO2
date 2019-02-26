c6361d3c82beac6e8dcaf3ce3335d60c8b9bdcc2
mingkaijiang <shanyujordan512@gmail.com>
2019-02-26 15:43:23 +1100

    done for the day

--- function/structural_equation_modeling.R
+++ function/structural_equation_modeling.R
@@ -13,19 +13,55 @@ structural_equation_modeling_test <- function(reDF) {
     
     sDF$Variable <- gsub(" ", "_", sDF$Variable)
     
-    testDF <- summaryBy(log_interaction~Variable+Vegetation_type, data=sDF, FUN=mean, keep.names=T)
-    
-    wdDF <- spread(data = testDF, 
+    wdDF <- spread(data = sDF, 
                  key = "Variable",
                  value = "log_interaction")
     
     
     ### structural equation
-    HS.model <- 'Biomass =~ Leaf_biomass + Root_biomass + Stem_biomass
-                 Concentration =~ Leaf_N_concentration + Root_N_concentration 
-                 Morphology =~ SLA + Total_root_length'
+    HS.model <- 'Total_plant_biomass ~ Aboveground_biomass + Belowground_biomass
+                 Aboveground_biomass ~ Leaf_biomass + Stem_biomass'
     
     fit <- cfa(HS.model, data = wdDF)
+    
+    
+    ### plot correlation
+    wilcox.test(wdDF$Aboveground_biomass, wdDF$Belowground_biomass, alternative = "two.sided")
+    
+    library("ggpubr")
+    ggboxplot(sDF, x = "Category", y = "log_interaction", 
+              color = "Category", 
+              ylab = "log(r)", xlab = "Category")
+    
+    
+    ### three-way anova
+    test <- subset(sDF, Variable == "Leaf_biomass")
+    testDF <- test[,c("Literature", "Vegetation_type", "Experiment_duration", "log_interaction")]
+    testDF$Literature <- as.factor(testDF$Literature)
+    testDF$Vegetation_type <- as.factor(testDF$Vegetation_type)
+    testDF$Experiment_duration <- as.factor(testDF$Experiment_duration)
+    
+    plot.design(log_interaction ~ ., data = testDF)
+    
+    fm <- aov(log_interaction ~ Literature * Vegetation_type * Experiment_duration, data=testDF)
+    summary(fm)
+    
+    ## obtain model without 3-way interaction
+    fm <- update(fm, . ~ . -Literature:Vegetation_type:Experiment_duration)
+    summary(fm)
+    
+    # remove 2-way anova
+    fm1 <- update(fm, .~Literature+Vegetation_type+Experiment_duration)
+    summary(fm1)
+    
+    # anova
+    anova(fm, fm1)
+    
+    model.tables(fm1,type="effects")
+    
+    op <-  par(mfrow = c(2, 2))
+    plot(fm1)
+    par(op)
     
     
 }
