5ffef1eeaf4ceb48cbb671df188b2e420dd80f3e
mingkaijiang <shanyujordan512@gmail.com>
2019-02-28 21:26:51 +1100

    end day

--- function/scenario_illustration_plot.R
+++ function/scenario_illustration_plot.R
@@ -44,20 +44,157 @@ scenario_illustration_plot <- function() {
         theme(panel.grid.minor=element_blank(),
               axis.title.x = element_text(size=12), 
               axis.text.x = element_text(size=12),
-              axis.text.y=element_blank(),
-              axis.title.y=element_blank(),
+              axis.text.y=element_text(size=12),
+              axis.title.y=element_text(size=14),
               legend.text=element_text(size=12),
               legend.title=element_text(size=14),
               panel.grid.major=element_blank(),
               legend.position="none",
               legend.text.align=0)+
-        scale_x_continuous(labels=c("1" = "aP", "2" = "eP1",
-                                  "3" = "eP2", "4" = "eP3",
-                                  "5" = "eP4"))
+        scale_x_continuous(limit=c(0, 5),
+                           labels=c("0" ="",
+                                    "1" = "aP", "2" = "eP1",
+                                    "3" = "eP2", "4" = "eP3",
+                                    "5" = "eP4"))+
+        annotate("text", x=0.7, y=1.08, label="15 %", size=4)+
+        annotate("segment", x = 0.9, xend = 0.9, y = 1, yend = 1.15,
+                 colour = "black", size=1)+
+        annotate("segment", x = 0.87, xend = 0.93, y = 1, yend = 1,
+                 colour = "black", size=1)+
+        annotate("segment", x = 0.87, xend = 0.93, y = 1.15, yend = 1.15,
+                 colour = "black", size=1)+
+        #annotate("text", x=0.7, y=1.4, label="30 %", size=4)+
+        #annotate("segment", x = 0.9, xend = 0.9, y = 1.17, yend = 1.5,
+        #         colour = "black", size=1)+
+        #annotate("segment", x = 0.87, xend = 0.93, y = 1.17, yend = 1.17,
+        #         colour = "black", size=1)+
+        #annotate("segment", x = 0.87, xend = 0.93, y = 1.5, yend = 1.5,
+        #         colour = "black", size=1)+
+        annotate("text", x=2.3, y=1.4, label="15 %", size=4)+
+        annotate("segment", x = 2.1, xend = 2.1, y = 1.3, yend = 1.5,
+                 colour = "black", size=1)+
+        annotate("segment", x = 2.07, xend = 2.13, y = 1.3, yend = 1.3,
+                 colour = "black", size=1)+
+        annotate("segment", x = 2.07, xend = 2.13, y = 1.5, yend = 1.5,
+                 colour = "black", size=1)+
+        #annotate("text", x=2.3, y=1.12, label="30 %", size=4)+
+        #annotate("segment", x = 2.1, xend = 2.1, y = 1.0, yend = 1.27,
+        #         colour = "black", size=1)+
+        #annotate("segment", x = 2.07, xend = 2.13, y = 1.0, yend = 1.0,
+        #         colour = "black", size=1)+
+        #annotate("segment", x = 2.07, xend = 2.13, y = 1.27, yend = 1.27,
+        #         colour = "black", size=1)
+        annotate("text", x=4.75, y=3.05, label="15 %", size=4)+
+        annotate("segment", x = 4.97, xend = 4.97, y = 2.86, yend = 3.28,
+                 colour = "black", size=1)+
+        annotate("segment", x = 4.94, xend = 5.0, y = 2.86, yend = 2.86,
+                 colour = "black", size=1)+
+        annotate("segment", x = 4.94, xend = 5.0, y = 3.28, yend = 3.28,
+                 colour = "black", size=1)
     
-    plot(p1)
+    ### plot  - elevated P effect at aC and eC
+    p2 <- ggplot() +  
+        geom_point(data=s2DF, 
+                   aes(P_trt, value, col=CO2_trt),
+                   shape=19, size=5)+
+        geom_line(data=s2DF, 
+                  aes(P_trt, value, col=CO2_trt), lty=2, lwd=1.5)+
+        geom_point(data=s2DF.sub, 
+                   aes(P_trt, value, col=CO2_trt),
+                   shape=19, size=5)+
+        geom_line(data=s2DF.sub, 
+                  aes(P_trt, value, col=CO2_trt), lwd=1.5)+
+        xlab("Phosphorus treatment") + 
+        ylab("Response") +
+        theme_linedraw() +
+        theme(panel.grid.minor=element_blank(),
+              axis.title.x = element_text(size=12), 
+              axis.text.x = element_text(size=12),
+              axis.text.y=element_text(size=12),
+              axis.title.y=element_text(size=14),
+              legend.text=element_text(size=12),
+              legend.title=element_text(size=14),
+              panel.grid.major=element_blank(),
+              legend.position="none",
+              legend.text.align=0)+
+        scale_x_continuous(limit=c(0, 5),
+                           labels=c("0" ="",
+                                    "1" = "aP", "2" = "eP1",
+                                    "3" = "eP2", "4" = "eP3",
+                                    "5" = "eP4"))+
+        annotate("text", x=0.7, y=1.08, label="15 %", size=4)+
+        annotate("segment", x = 0.9, xend = 0.9, y = 1, yend = 1.15,
+                 colour = "black", size=1)+
+        annotate("segment", x = 0.87, xend = 0.93, y = 1, yend = 1,
+                 colour = "black", size=1)+
+        annotate("segment", x = 0.87, xend = 0.93, y = 1.15, yend = 1.15,
+                 colour = "black", size=1)+
+        annotate("text", x=2.3, y=1.4, label="20 %", size=4)+
+        annotate("segment", x = 2.1, xend = 2.1, y = 1.3, yend = 1.56,
+                 colour = "black", size=1)+
+        annotate("segment", x = 2.07, xend = 2.13, y = 1.3, yend = 1.3,
+                 colour = "black", size=1)+
+        annotate("segment", x = 2.07, xend = 2.13, y = 1.56, yend = 1.56,
+                 colour = "black", size=1)+
+        annotate("text", x=4.75, y=5.3, label="35 %", size=5)+
+        annotate("segment", x = 4.97, xend = 4.97, y = 4.37, yend = 5.9,
+                 colour = "black", size=1)+
+        annotate("segment", x = 4.94, xend = 5.0, y = 4.37, yend = 4.37,
+                 colour = "black", size=1)+
+        annotate("segment", x = 4.94, xend = 5.0, y = 5.9, yend = 5.9,
+                 colour = "black", size=1)
     
-    
+    ### plot  - elevated P effect at aC and eC
+    p3 <- ggplot() +  
+        geom_point(data=s3DF, 
+                   aes(P_trt, value, col=CO2_trt),
+                   shape=19, size=5)+
+        geom_line(data=s3DF, 
+                  aes(P_trt, value, col=CO2_trt), lty=2, lwd=1.5)+
+        geom_point(data=s3DF.sub, 
+                   aes(P_trt, value, col=CO2_trt),
+                   shape=19, size=5)+
+        geom_line(data=s3DF.sub, 
+                  aes(P_trt, value, col=CO2_trt), lwd=1.5)+
+        xlab("Phosphorus treatment") + 
+        ylab("Response") +
+        theme_linedraw() +
+        theme(panel.grid.minor=element_blank(),
+              axis.title.x = element_text(size=12), 
+              axis.text.x = element_text(size=12),
+              axis.text.y=element_text(size=12),
+              axis.title.y=element_text(size=14),
+              legend.text=element_text(size=12),
+              legend.title=element_text(size=14),
+              panel.grid.major=element_blank(),
+              legend.position="bottom",
+              legend.text.align=0)+
+        scale_x_continuous(limit=c(0, 5),
+                           labels=c("0" ="",
+                                    "1" = "aP", "2" = "eP1",
+                                    "3" = "eP2", "4" = "eP3",
+                                    "5" = "eP4"))+
+        annotate("text", x=0.7, y=1.08, label="15 %", size=4)+
+        annotate("segment", x = 0.9, xend = 0.9, y = 1, yend = 1.15,
+                 colour = "black", size=1)+
+        annotate("segment", x = 0.87, xend = 0.93, y = 1, yend = 1,
+                 colour = "black", size=1)+
+        annotate("segment", x = 0.87, xend = 0.93, y = 1.15, yend = 1.15,
+                 colour = "black", size=1)+
+        annotate("text", x=2.3, y=1.38, label="10 %", size=4)+
+        annotate("segment", x = 2.1, xend = 2.1, y = 1.3, yend = 1.43,
+                 colour = "black", size=1)+
+        annotate("segment", x = 2.07, xend = 2.13, y = 1.3, yend = 1.3,
+                 colour = "black", size=1)+
+        annotate("segment", x = 2.07, xend = 2.13, y = 1.43, yend = 1.43,
+                 colour = "black", size=1)+
+        annotate("text", x=4.75, y=1.7, label="-5 %", size=5)+
+        annotate("segment", x = 4.97, xend = 4.97, y = 1.63, yend = 1.72,
+                 colour = "black", size=1)+
+        annotate("segment", x = 4.94, xend = 5.0, y = 1.63, yend = 1.63,
+                 colour = "black", size=1)+
+        annotate("segment", x = 4.94, xend = 5.0, y = 1.72, yend = 1.72,
+                 colour = "black", size=1)
     
     
     ### Plotting
